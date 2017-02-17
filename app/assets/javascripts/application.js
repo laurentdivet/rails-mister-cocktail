058454fa0f0b0ba97dfc995c214fc105ae6f42c1
laurentdivet <laurentdivet@icloud.com>
2017-02-17 11:18:59 +0100

    mister cocktail Tomorow

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,10 +13,7 @@
 //= require jquery
 //= require jquery_ujs
 //= require turbolinks
-//= require_tree .
-// app/assets/javascripts/application.js
-
-//= require jquery
-//= require jquery_ujs
 //= require bootstrap-sprockets
 //= require_tree .
+
+
